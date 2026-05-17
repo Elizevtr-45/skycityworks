f00445a0d2659b425747757d64b9121db67541f5
Elizevtr-45 <agnessa.fris@gmail.com>
2026-05-17 23:38:01 +1000

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tsr: {
-    disableImportProtection: true
-  }
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart()
+  ]
 });
